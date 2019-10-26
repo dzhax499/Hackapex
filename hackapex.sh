825fe658f1590d32ebe1456dbba74257329d13a5
dzhax499 <57004084+dzhax499@users.noreply.github.com>
2019-10-26 09:37:04 +0700

    Add files via upload

---
+++ hackapex.sh
@@ -0,0 +1,46 @@
+clear
+echo "initializing..."
+sleep 20
+echo "Injecting..."
+sleep 20
+echo "Success!"
+sleep 5
+clear
+echo "To Get license key , Subscribe my Channel [YtDzakir] and like all video"
+sleep 5
+echo "This cheat IS free!"
+sleep 3
+echo "And see to My Script!"
+sleep 3
+echo "[apt install nano / sudo apt install nano]"
+echo "nano hackapex.sh"
+echo "Search my Lisence key!"
+read -p "license key? ==> " key;
+sleep 10
+if [ $key = "DzHax499" ];
+then
+    echo "Okey!"
+    sleep 9
+    echo "Downloading cheat..."
+    clear
+    sleep 10
+    read -p "What is Your name??==> " name;
+    echo "Login with $name"
+    sleep 10
+    echo "waiting for apex legends..."
+    sleep 60
+    echo "  "
+    toilet -f big -F gay SUCCESS!  enjoy your cheating..
+    echo "press 'insert' to open cheat"
+    echo "F1  ===   Aimbot / trigger bot"
+    echo "F2  ===   Esp / enemy or chams"
+    echo "F9  ===   [esp]Bone / line / glow esp"
+    echo "F10 ===   [Aimbot] Head / body / buttom"
+    echo "[CTRL + C ] TO EXIT TOOLS"
+    sleep 5
+    echo "SUBSCRIBE MY CHANNEL YtDzakir"
+    sleep 120
+    exit
+else
+        echo "Wrong Key!"
+fi
